89ac590de86edb8f72af4491b02a2093faebfb24
Akhmad Zaki <akhmadzaki2@gmail.com>
2022-01-19 21:04:20 +0700

    Added Day 2 - 2015 solutions

---
+++ 2015/day-2/part1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ifstream inFile("in.txt");
+
+    vector<string> dimensions;
+    string dimension;
+
+    while(getline(inFile,dimension)) //Read puzzle input line by line
+    { 
+        dimensions.push_back(dimension); //Insert each line into the vector for further processing
+    }
+
+    int wrappingPaper = 0;
+
+    for(auto dimension:dimensions) //Process each line of present's dimension
+    {
+        stringstream dims(dimension);
+        string dim;
+        vector<int> boxDim;
+        while(getline(dims,dim,'x')) //Split each line of box dimension by delimiter 'x'
+        {
+            boxDim.push_back(stoi(dim));
+        }
+        sort(boxDim.begin(),boxDim.end()); //Sort the box dim value
+        wrappingPaper+=2*boxDim[0]*boxDim[1]+2*boxDim[1]*boxDim[2]+2*boxDim[0]*boxDim[2] + boxDim[0]*boxDim[1]; //Surface area of present + Area of smallest side 
+    }
+    cout << wrappingPaper << endl;
+}
---
+++ 2015/day-2/readme.txt
@@ -0,0 +1,4 @@
+problem's link : https://adventofcode.com/2015/day/2
+reference: https://www.javatpoint.com/how-to-split-strings-in-cpp --split string by character/delimiter in C++
+
+note: puzzle input (in.txt) will be unique for each users. 
